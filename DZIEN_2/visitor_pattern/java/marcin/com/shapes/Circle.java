9c19f08d43159420c9e89d24b927216895c520c1
albim72 <marcin.albiniak72@gmail.com>
2023-08-30 11:49:11 +0200

    Create Circle.java

---
+++ DZIEN_2/visitor_pattern/java/marcin/com/shapes/Circle.java
@@ -0,0 +1,21 @@
+package marcin.com.shapes;
+
+import marcin.com.visitor.Visitor;
+public class Circle extends Dot {
+    
+    private int radius;
+
+    public Circle(int id, int x, int y, int radius) {
+        super(id, x, y);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String accept(Visitor visitor) {
+        return visitor.visitCircle(this);
+    }
+}
