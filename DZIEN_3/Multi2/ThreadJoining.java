73493551261b3d74dfd3f6974ce2f3029a60177a
albim72 <marcin.albiniak72@gmail.com>
2023-08-31 14:24:12 +0200

    Create ThreadJoining.java

---
+++ DZIEN_3/Multi2/ThreadJoining.java
@@ -0,0 +1,15 @@
+public class ThreadJoining extends Thread{
+    @Override
+    public void run() {
+        for (int i=0;i<2;i++) {
+            try {
+                Thread.sleep(500);
+                System.out.println("Aaktualny wątek: " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i); 
+        }
+        
+    }
+}
