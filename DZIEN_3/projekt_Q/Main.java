aa34f2707fcdd68b517765f02dffc5b9311b093d
albim72 <marcin.albiniak72@gmail.com>
2023-08-31 15:58:05 +0200

    Create Main.java

---
+++ DZIEN_3/projekt_Q/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Q q = new Q();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+
+        p.t.start();
+        c.t.start();
+        System.out.println("Naciśnij CTRL+F2 aby zakończyć!");
+
+    }
