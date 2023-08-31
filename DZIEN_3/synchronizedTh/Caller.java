9dd8885f184e1d4130dd636426b14d9ffe7276e5
albim72 <marcin.albiniak72@gmail.com>
2023-08-31 15:39:12 +0200

    Create Caller.java

---
+++ DZIEN_3/synchronizedTh/Caller.java
@@ -0,0 +1,18 @@
+public class Caller implements Runnable{
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(String msg, Callme target) {
+        this.msg = msg;
+        this.target = target;
+        t = new Thread(this);
+    }
+
+    @Override
+    public void run() {
+        synchronized (target){
+            target.call(msg);
+        }
+    }
+}
