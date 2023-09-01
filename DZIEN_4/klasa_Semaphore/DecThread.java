def4289086a121e4ad797b512476555ace1c8b4d
albim72 <marcin.albiniak72@gmail.com>
2023-09-01 09:55:12 +0200

    Create DecThread.java

---
+++ DZIEN_4/klasa_Semaphore/DecThread.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.Semaphore;
+
+public class DecThread implements Runnable{
+    String name;
+    Semaphore sem;
+
+    public DecThread(String name, Semaphore sem) {
+        this.name = name;
+        this.sem = sem;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Uruchamiam: " + name);
+        try{
+            System.out.println(name + " czeka na pozwolenie.");
+            sem.acquire();
+            System.out.println(name + " uzyskał pozwolenie.");
+            //dostęp do zasobów udostępnionych
+            for(int i=0;i<5;i++){
+                Shared.count--;
+                System.out.println(name + ": " + Shared.count);
+                Thread.sleep(10);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(name + " zwalnia pozwolenie na dostęp.");
+        sem.release();
+    }
+}
