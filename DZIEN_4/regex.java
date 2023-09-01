d756c25ed92bb256d7c21d242487581e7c611f5c
albim72 <marcin.albiniak72@gmail.com>
2023-09-01 15:21:10 +0200

    Create regex.java

---
+++ DZIEN_4/regex.java
@@ -0,0 +1,46 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Pattern pat;
+        Matcher mat;
+        boolean found;
+
+        pat = Pattern.compile("Java");
+        mat = pat.matcher("Java");
+        found = mat.matches();
+        System.out.println("dopasowanie słowa zadanego i sprawdzanego... opcja 1 ->");
+        if(found) System.out.println("Pasuje");
+        else System.out.println("Nie pasuje");
+
+        System.out.println("__________________________________");
+        mat = pat.matcher("Java SE");
+        found = mat.matches();
+        System.out.println("dopasowanie słowa zadanego i sprawdzanego... opcja 2 ->");
+        if(found) System.out.println("Pasuje");
+        else System.out.println("Nie pasuje");
+
+        System.out.println("__________________________________");
+
+        pat = Pattern.compile("W+");
+        mat = pat.matcher("W WW WWW WWWWWWW WW_WW");
+        while (mat.find())
+            System.out.println("Dopasowana sekwencja: " + mat.group());
+
+        System.out.println("__________________________________");
+
+        pat = Pattern.compile("e.+d");
+        mat = pat.matcher("extend cup end table");
+        while (mat.find())
+            System.out.println("Dopasowana sekwencja: " + mat.group());
+
+        System.out.println("__________________________________");
+
+        pat = Pattern.compile("e.+?d");
+        mat = pat.matcher("extend cup end table");
+        while (mat.find())
+            System.out.println("Dopasowana sekwencja: " + mat.group());
+
+    }
+}
