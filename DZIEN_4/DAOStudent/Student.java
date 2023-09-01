fd8910056f10bb168b51953a402893cbe8d4d3a3
albim72 <marcin.albiniak72@gmail.com>
2023-09-01 13:43:07 +0200

    Create Student.java

---
+++ DZIEN_4/DAOStudent/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name;
+    private int nralb;
+
+    public Student(String name, int nralb) {
+        this.name = name;
+        this.nralb = nralb;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNralb() {
+        return nralb;
+    }
+
+    public void setNralb(int nralb) {
+        this.nralb = nralb;
+    }
+}
